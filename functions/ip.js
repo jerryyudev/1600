1d744c33d8fe6fb2cc840b5171dd7d9742da2d0c
jerryyudev <118990409+jerryyudev@users.noreply.github.com>
2025-03-07 21:02:13 +0800

    Update ip.js

--- functions/ip.js
+++ functions/ip.js
@@ -1,28 +1,30 @@
 export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  const ip = url.pathname.split('/')[1];
+    const url = new URL(context.request.url);
+    const pathParts = url.pathname.split("/").filter(Boolean); // 过滤空值
 
-  if (!ip) {
-    return new Response('IP address is required', { status: 400 });
-  }
-
-  const apiUrl = `http://ip-api.com/json/${ip}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query`;
-
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-
-    if (data.status === 'fail') {
-      return new Response(
-        JSON.stringify({ status: 'fail', message: data.message, query: ip }, null, 2),
-        { headers: { 'Content-Type': 'application/json' } }
-      );
+    if (pathParts.length < 2 || !pathParts[1]) {
+        return new Response(JSON.stringify({
+            status: "fail",
+            message: "invalid query",
+            query: "missing"
+        }), { headers: { "Content-Type": "application/json" }, status: 400 });
     }
 
-    return new Response(JSON.stringify(data, null, 2), {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  } catch (error) {
-    return new Response('Error fetching IP data', { status: 500 });
-  }
+    const ip = pathParts[1];
+
+    const apiUrl = `http://ip-api.com/json/${ip}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        return new Response(JSON.stringify(data, null, 2), {
+            headers: { "Content-Type": "application/json" }
+        });
+    } catch (error) {
+        return new Response(JSON.stringify({
+            status: "fail",
+            message: "API error"
+        }), { headers: { "Content-Type": "application/json" }, status: 500 });
+    }
 }
